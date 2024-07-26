dfaef5058e92b7480de118d6aa5557f69b1b6803
arshedgithub <arshedahmedmail@gmail.com>
2024-07-26 07:17:11 +0530

    add weather Adapter class

--- app/src/main/java/com/example/weatherapp/Weather.java
+++ app/src/main/java/com/example/weatherapp/Weather.java
@@ -2,11 +2,11 @@ package com.example.weatherapp;
 
 public class Weather {
 
-    public String icon;
+    public int icon;
     public String weatherDate;
     public String temp;
 
-    public Weather(String icon, String weatherDate, String temp) {
+    public Weather(int icon, String weatherDate, String temp) {
         this.icon = icon;
         this.weatherDate = weatherDate;
         this.temp = temp;
---
+++ app/src/main/java/com/example/weatherapp/WeatherAdapter.java
@@ -0,0 +1,43 @@
+package com.example.weatherapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class WeatherAdapter extends ArrayAdapter {
+
+    private ArrayList<Weather> weatherArrayList;
+
+    public WeatherAdapter(@NonNull Context context, ArrayList<Weather> weatherArrayList) {
+        super(context, R.layout.weather_forecast_item, weatherArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Weather weatherData = (Weather) getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.weather_forecast_item, parent, false);
+        }
+
+        ImageView weatherIcon = convertView.findViewById(R.id.weather_icon);
+        TextView weatherDate = convertView.findViewById(R.id.text_date);
+        TextView weatherTemp = convertView.findViewById(R.id.text_temp);
+
+        weatherIcon.setImageResource(weatherData.icon);
+        weatherDate.setText(weatherData.weatherDate);
+        weatherTemp.setText(weatherData.temp);
+
+        return convertView;
+    }
+}
